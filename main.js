dbd56e7198981a7a599225c8f529a375982d6526
Charykarthik <128477106+Charykarthik@users.noreply.github.com>
2023-04-15 14:25:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function storage(line)
 {
 var values = line.split(',');
 var obj = new Object();
-obj.s.no= values[0];
+obj.sno= values[0];
 obj.Emp_name= values[1];
 obj.Emp_id= values[2];
 obj.location= values[3];
